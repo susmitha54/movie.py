bd590c6e2e5f3a251cf896a54e08952b1aa4462c
susmitha54 <143336106+susmitha54@users.noreply.github.com>
2023-10-18 18:15:21 +0530

    Update movie.py

--- movie.py
+++ movie.py
@@ -4,9 +4,6 @@
 #Print "You both like "name of the movie"
 #Continue until they is atleast 3 movies they both like
 
-#init variables
-
-
 
 movies=[]
 common_movies=[]
@@ -22,21 +19,3 @@ for movie in movies:
            same_movies.append(movie)  
 print("movies:",movies)       
 print("You both like:",same_movies)
-
-
-'''
-fruit_basket=[]
-unique_fruits=[]
-repeat_fruits=[]
-for i in range(4):
-    fruit=input('Enter fruits:')
-    fruit_basket.append(fruit)
-for fruit in fruit_basket:
-    if fruit not in unique_fruits:
-        unique_fruits.append(fruit)
-    else:
-      if fruit not in repeat_fruits:
-        repeat_fruits.append(fruit)
-print('Unique fruits:',unique_fruits)
-print('Repeated fruits:',repeat_fruits)
-'''
